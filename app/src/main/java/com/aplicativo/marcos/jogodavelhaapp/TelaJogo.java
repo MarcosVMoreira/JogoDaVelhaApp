c39379bc9e75b8374df6739166e9a74100f051d4
Marcos Moreira <contato.marcosvmoreira@gmail.com>
2019-03-19 09:33:17 -0300

    Add: estrutura do app e formato do jogo da velha

---
+++ app/src/main/java/com/aplicativo/marcos/jogodavelhaapp/TelaJogo.java
@@ -0,0 +1,13 @@
+package com.aplicativo.marcos.jogodavelhaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaJogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_jogo);
+    }
+}
